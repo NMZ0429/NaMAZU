a34de89a1283ec5acde3a35d8a20e86b5fc36cc0
NMZ0429 <gen0429@icloud.com>
2021-09-06 23:01:10 +0900

    add utilities for file management

--- NaMAZU/functional/__init__.py
+++ NaMAZU/functional/__init__.py
@@ -1,4 +1,12 @@
 from .image_control import *
+from .file_control import *
 
-__all__ = ["img_to_npy", "npy_to_img", "split_image", "apply_to_all"]
+__all__ = [
+    "img_to_npy",
+    "npy_to_png",
+    "split_image",
+    "apply_to_all",
+    "collect_file_pathes_by_ext",
+    "zip_files",
+]
 
