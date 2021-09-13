a96c368760e5c35a89482fc584dc96835b97048b
NMZ0429 <gen0429@icloud.com>
2021-09-13 23:31:26 +0900

    add usefull method

--- NaMAZU/lightning_wingman/lit_u2net.py
+++ NaMAZU/lightning_wingman/lit_u2net.py
@@ -1,15 +1,18 @@
 from os.path import sep
+from pathlib import Path
 from typing import List, Tuple
 
+import cv2
 import numpy as np
 import torch
+from NaMAZU.functional.image_control import apply_mask_to
 from PIL import Image
+from PIL.Image import Image as PILImage
 from pytorch_lightning import LightningModule
 from torch import Tensor
 from torchvision import transforms
 
 from .u2net import U2NET, U2NETP, RescaleT, ToTensorLab
-
 
 __all__ = ["LitU2Net"]
 
@@ -27,12 +30,15 @@ class LitU2Net(LightningModule):
         super().__init__(*args, **kwargs)
         self.save_hyperparameters()
         self.__load_model()
+        self.preprocess = transforms.Compose([RescaleT(320), ToTensorLab(flag=0)])
 
-        if not train_model:
+        if train_model:
             self.model.train()
             self.bce_loss = torch.nn.BCELoss(size_average=True)
 
     def forward(self, x: Tensor) -> Tuple[Tensor, ...]:
+        if x.dim() == 3:
+            x = x.unsqueeze(0)
         x = x.to(self.device)
         return self.model(x)
 
@@ -49,18 +55,18 @@ class LitU2Net(LightningModule):
     def training_step(self, batch, batch_idx):
         x, y = batch
         y_hat = self.forward(x)
-        leading_loss, loss = self.__muti_bce_loss_fusion(*y_hat, labels_v=y)
+        leading_loss, loss = self.__multi_bce_loss_fusion(*y_hat, labels_v=y)
 
         return {"loss": loss, "log": {"train_loss": loss, "train_tar": leading_loss}}
 
     def validation_step(self, batch, batch_idx):
         x, y = batch
         y_hat = self.forward(x)
-        leading_loss, loss = self.__muti_bce_loss_fusion(*y_hat, labels_v=y)
+        leading_loss, loss = self.__multi_bce_loss_fusion(*y_hat, labels_v=y)
 
         return {"val_loss": loss, "log": {"val_loss": loss, "val_tar": leading_loss}}
 
-    def __muti_bce_loss_fusion(self, d0, d1, d2, d3, d4, d5, d6, labels_v):
+    def __multi_bce_loss_fusion(self, d0, d1, d2, d3, d4, d5, d6, labels_v):
 
         loss0 = self.bce_loss(d0, labels_v)
         loss1 = self.bce_loss(d1, labels_v)
@@ -87,9 +93,7 @@ class LitU2Net(LightningModule):
         return loss0, loss
 
     def predict(self, x_path: str, save: bool = False, save_path: str = "",) -> Tensor:
-        x = Image.open(x_path)
-        x = transforms.ToTensor()(x).unsqueeze_(0)
-        x = torch.tensor(x)
+        x = cv2.imread(x_path)
         x = self.__input_preprocess(x)
         d1 = self.forward(x)[0]
         pred = d1[:, 0, :, :]
@@ -117,7 +121,7 @@ class LitU2Net(LightningModule):
         img_name = original_image.split(sep)[-1]
         # image = io.imread(original_image)
         image = Image.open(original_image)
-        imo = im.resize((image.size[1], image.size[0]), resample=Image.BILINEAR)
+        imo = im.resize((image.size[0], image.size[1]), resample=Image.BILINEAR)
 
         pb_np = np.array(imo)
 
@@ -127,12 +131,24 @@ class LitU2Net(LightningModule):
         for i in range(1, len(bbb)):
             imidx = imidx + "." + bbb[i]
 
-        imo.save(d_dir + imidx + ".png")
+        out_dir = Path(d_dir)
+        if not out_dir.exists():
+            out_dir.mkdir(parents=True)
 
-    def __input_preprocess(self, x: Tensor):
-        trans = transforms.Compose([RescaleT(320), ToTensorLab(flag=0)])
+        imo.save(out_dir / (imidx + ".png"))
 
-        return trans(x)
+    def apply_mask(self, prediction: Tensor, original_image: str) -> PILImage:
+        predict_np = prediction.squeeze().cpu().data.numpy()
+
+        im = Image.fromarray(predict_np * 255).convert("RGB")
+        image = Image.open(original_image)
+        mask = im.resize((image.size[0], image.size[1]), resample=Image.BILINEAR)
+
+        return apply_mask_to(image, mask)
+
+    def __input_preprocess(self, x: PILImage) -> Tensor:
+        processed = self.preprocess(x).type(torch.FloatTensor)  # type: ignore
+        return processed
 
     def __load_model(self) -> None:
         """Download checkpoint file and load the model.
@@ -157,3 +173,11 @@ class LitU2Net(LightningModule):
             )
         self.model.load_state_dict(state_dict=st_dict)
         self.model.eval()
+
+    def debug_method(self):
+        # pred = torch.load("boat_pred.pth")
+        inp = torch.load("boat_input.pth")
+        d1 = self.forward(inp)[0]
+        pred = d1[:, 0, :, :]
+        pred = self.__normPRED(pred)
+        self.__save_output("boat.jpg", pred, "debug-")
