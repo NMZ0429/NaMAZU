7b939a6086b481deeb0a4b2bf71321416f1b6233
NMZ0429 <gen0429@icloud.com>
2021-09-18 23:35:26 +0900

    version update

--- NaMAZU/__init__.py
+++ NaMAZU/__init__.py
@@ -2,7 +2,7 @@ __version__ = "0.0.38"
 
 import NaMAZU.lightning_wingman
 import NaMAZU.functional
-import st_integration
+import NaMAZU.st_integration
 
 # rm -rf build dist NaMAZU.egg-info
 # python setup.py bdist_wheel
--- build/lib/NaMAZU/__init__.py
+++ build/lib/NaMAZU/__init__.py
@@ -1,7 +1,8 @@
-__version__ = "0.0.37"
+__version__ = "0.0.38"
 
 import NaMAZU.lightning_wingman
 import NaMAZU.functional
+import NaMAZU.st_integration
 
 # rm -rf build dist NaMAZU.egg-info
 # python setup.py bdist_wheel
