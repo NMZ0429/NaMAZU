2fccc73083d8feca4d7df66aefc88aaeb402a018
NMZ <gen0429@icloud.com>
2021-09-19 13:38:28 +0900

    update version

--- setup.py
+++ setup.py
@@ -15,6 +15,26 @@ PYTHON_REQUIRES = ">=3.8.0"
 INSTALL_REQUIRES = ["numpy", "pillow", "pytorch_lightning"]
 
 PACKAGES = setuptools.find_packages()
+CLASSIFIERS = [
+    "Environment :: Console",
+    "Natural Language :: English",
+    # How mature is this project? Common values are
+    #   3 - Alpha, 4 - Beta, 5 - Production/Stable
+    "Development Status :: 4 - Beta",
+    # Indicate who your project is intended for
+    "Intended Audience :: Developers",
+    "Topic :: Scientific/Engineering :: Artificial Intelligence",
+    "Topic :: Scientific/Engineering :: Image Recognition",
+    "Topic :: Scientific/Engineering :: Information Analysis",
+    # Pick your license as you wish
+    "License :: OSI Approved :: Apache Software License",
+    "Operating System :: OS Independent",
+    # Specify the Python versions you support here. In particular, ensure
+    # that you indicate whether you support Python 2, Python 3 or both.
+    "Programming Language :: Python :: 3",
+    "Programming Language :: Python :: 3.8",
+    "Programming Language :: Python :: 3.9",
+]
 
 with open("README.md", "r") as fp:
     readme = fp.read()
@@ -37,6 +57,6 @@ setup(
     install_requires=INSTALL_REQUIRES,
     # extras_require=EXTRAS_REQUIRE,
     packages=PACKAGES,
-    # classifiers=CLASSIFIERS,
+    classifiers=CLASSIFIERS,
 )
 
