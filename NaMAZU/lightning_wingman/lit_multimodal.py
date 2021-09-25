34e65b72726d141a1b9d08eb6b05d55b743611af
NMZ0429 <gen0429@icloud.com>
2021-09-25 15:07:15 +0900

    add multimodal network

--- NaMAZU/lightning_wingman/lit_multimodal.py
+++ NaMAZU/lightning_wingman/lit_multimodal.py
@@ -1,9 +1,11 @@
 from torch import Tensor
 import torch
-from typing import List, Union, Dict
+from typing import List, Optional, Union, Dict
 from torch.optim import Adam
 from torch.optim.lr_scheduler import StepLR
 from pytorch_lightning import LightningModule
+
+from .multi_modal import StochasticFusion
 
 
 class MultiModalNet(LightningModule):
@@ -16,8 +18,9 @@ class MultiModalNet(LightningModule):
         num_classes: int = 1,
         is_regression: bool = False,
         train_encoders: bool = False,
-        use_default_modality_dropout: bool = True,
+        use_modality_dropout: bool = True,
         md_prob: float = 0.5,
+        use_stochastic_fusion: bool = False,
         **kwargs,
     ) -> None:
         """Multipurpose multimodal network.
@@ -25,14 +28,14 @@ class MultiModalNet(LightningModule):
         This model operates stochastic modality sampling to increase robustness of fusion vector.
         Is consists of three parts:
             1. Encoder:
-                List of encoder modules for each modality. The choice of encoder is determined by the user depending on modalities.
+                List of encoder models for each modality. The choice of encoder is determined by the user depending on modalities.
             2. Fusion layers:
-                Main part of the model.They are the list of linear layersa and one sampling function.
+                Main part of the model. They are the list of linear layers and one sampling function.
                 They takes the list of vectors from encoders and fuse them stochastically.
                 There are two adjustable parameters for the fusion layers:
-                    1. modal_available:
-                        This is a tensor of shape (batch_size, num_modalities) which indicates whether the modality is available or not. 
-                        Use built-in modality droput function to utilize this and make the model more robust.
+                    1. modal_state:
+                        This is a tensor of shape (batch_size, num_modalities) which indicates whether the modality is given or not. 
+                        Use built-in modality droput functionality to utilize this and make the model more robust.
                     2. prior_modality_distribution:
                         This is a tensor of of shape [batch_size, num_modalities], which indicates the prior distribution of feature importance of each modality.
                         Changing this dynamically can improve the performance of the model and there is a built-in adjustment algorithm for this. But you can 
@@ -57,8 +60,9 @@ class MultiModalNet(LightningModule):
             num_classes (int, optional): Number of classes for classification task. Defaults to 1.
             is_regression (bool, optional): True to train it with MSE. Don't forget to set num_classes to 1. Defaults to False.
             train_encoders (bool, optional): True to train encoders too. Defaults to False.
-            use_default_modality_dropout (bool, optional): True to use default modality dropout. Defaults to True.
+            use_modality_dropout (bool, optional): True to use default modality dropout and stochastic fusion. Defaults to True.
             md_prob (float, optional): Probability of modality dropout. Defaults to 0.5.
+            use_stochastic_fusion (bool, optional): True to use stochastic fusion. Defaults to False.
         """
         super().__init__()
         self.save_hyperparameters()
@@ -78,17 +82,20 @@ class MultiModalNet(LightningModule):
         if not train_encoders:
             self.encoders.eval()
 
-    def forward(self, x_list: List[Tensor], is_train: bool = False) -> Tensor:
+        if use_stochastic_fusion:
+            self.fusioner = StochasticFusion(self.num_modalities)
+        else:
+            self.fusioner = None
+
+    def forward(self, x_list: List[Tensor], ma=None, prior=None) -> Tensor:
         self._varify_batch(x_list)
 
-        if is_train:
-            ma = self.modality_dropout()
-            prior = self.stochstic_fusion()
-        else:
-            ma, prior = None, None
+        if self.fusioner is not None:
+            prior = self.stochstic_fusion(len(x_list))
+
         y_list = [encoder(x) for encoder, x in zip(self.encoders, x_list)]
         y_stack = self._by_modal_forward(y_list)
-        availability = self._check_lacking_modality(y_stack, modal_available=ma)
+        availability = self._check_lacking_modality(y_stack, modal_state=ma)
         prior_modality_distribution = self._check_prior(
             prior_modality_distribution=prior
         )
@@ -97,6 +104,18 @@ class MultiModalNet(LightningModule):
         )
 
         return self.output_layer(fusion_vector)
+
+    def training_step(self, batch):
+        x_list, y = batch
+
+        if self.hparams.use_modality_dropout:  # type: ignore
+            ma = self.modality_dropout(len(x_list), self.num_modalities)
+        else:
+            ma = None
+        y_hat = self(x_list, ma)
+        loss = self.loss(y_hat, y)
+
+        return {"loss": loss}
 
     def configure_optimizers(self,) -> Dict[str, Union[Adam, StepLR]]:
         params = []
@@ -131,17 +150,14 @@ class MultiModalNet(LightningModule):
         )  # [batch_size, latent_dim, num_modalities]
 
     def _check_lacking_modality(
-        self, modalities: Tensor, modal_available: Union[Tensor, None] = None
+        self, X: Tensor, modal_state: Union[Tensor, None] = None
     ) -> Tensor:
-        if modal_available is None:
+        if modal_state is None:
             return torch.ones(
-                modalities.shape[0],
-                self.num_modalities,
-                dtype=torch.float,
-                device=self.device,
+                X.shape[0], self.num_modalities, dtype=torch.float, device=self.device,
             )
         else:
-            return modal_available
+            return modal_state
 
     def _check_prior(
         self, prior_modality_distribution: Union[Tensor, None] = None
@@ -157,9 +173,9 @@ class MultiModalNet(LightningModule):
         self,
         laten_modalities: Tensor,
         prior_modality_distribution: Tensor,
-        modal_available: Tensor,
+        modal_state: Tensor,
     ):
-        weighted_prior = torch.mul(prior_modality_distribution, modal_available)
+        weighted_prior = torch.mul(prior_modality_distribution, modal_state)
         weighted_prior = torch.div(prior_modality_distribution, weighted_prior)
 
         modality_idx = torch.multinomial(
@@ -192,15 +208,29 @@ class MultiModalNet(LightningModule):
 
         self.encoders = torch.nn.ModuleList(self.hparams.modality_encoders)  # type: ignore
 
-    # TODO: Add stochastic dropout
-    # TODO: update forward method to support above changes
+    # Optional Operations
 
-    def modality_dropout(self):
+    def modality_dropout(
+        self, batch_size: int, num_modalities: int
+    ) -> Union[Tensor, None]:
+        """Return a tensor of shape (batch_size, num_modalities) whose values are either 0 or 1.
+
+        Args:
+            batch_size (int): The batch size.
+            num_modalities (int): Number of modalities.
+
+        Returns:
+            Union[Tensor, None]: Dropped modalities or None if the dropout is disabled.
+        """
         drop = torch.rand(1).item()
         if drop > self.hparams.md_prob:  # type: ignore
-            return torch.zeros(
-                self.num_modalities, dtype=torch.float, device=self.device
-            )
+            return torch.round(torch.rand([batch_size, num_modalities]))
+        else:
+            return None
 
-    def stochstic_fusion(self):
-        pass
+    def stochstic_fusion(self, batch_size: int) -> Tensor:
+        w_list = [self.fusioner().sample() for _ in range(batch_size)]  # type: ignore
+        ws = torch.stack(w_list, dim=0)
+
+        return torch.nn.functional.softmax(ws, dim=1)  # type: ignore
+
