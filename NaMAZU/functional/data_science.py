46884350c532c14322a16c4c1b5cfd23662bb4dd
NMZ0429 <gen0429@icloud.com>
2021-09-28 07:06:24 +0900

    update docstring

--- NaMAZU/functional/data_science.py
+++ NaMAZU/functional/data_science.py
@@ -8,8 +8,15 @@ __all__ = ["train_linear_regressor"]
 def train_linear_regressor(df: pd.DataFrame, x: Union[str, List[str]], y: str):
     """
     Trains a linear regression model using the sklearn library.
+
+    Args:
+        df (pd.DataFrame): The dataframe to train the model on.
+        x (Union[str, List[str]]): Single or list of column names to use as features.
+        y (str): The column name of the target variable.
+
+    Returns:
+        sklearn.linear_model.LinearRegression: The trained model.
     """
     regressor = LinearRegression()
     regressor.fit(df[x].values, df[y].values)
     return regressor
-
