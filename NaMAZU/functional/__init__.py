f57a01a013b195b0217f3463b1f774504bce671f
NMZ0429 <gen0429@icloud.com>
2021-10-10 17:41:46 +0900

    test no all variable

--- NaMAZU/functional/__init__.py
+++ NaMAZU/functional/__init__.py
@@ -3,30 +3,3 @@ from .file_control import *
 from .data_science import *
 from .text_control import *
 from .coreml_control import *
-
-__all__ = [
-    "train_linear_regressor",
-    "rename_file",
-    "collect_file_pathes_by_ext",
-    "zip_files",
-    "img_to_npy",
-    "npy_to_png",
-    "apply_mask_to",
-    "split_image",
-    "compose_two_png",
-    "apply_to_all",
-    "change_frame_rates",
-    "save_all_frames",
-    "export_list_str_as",
-    "search_word_from",
-    "collect_images",
-    "drop_negative",
-    "randomly_choose_files",
-    "calculate_sample_stats",
-    "error_bound_of_mean",
-    "estimated_total",
-    "error_bound_of_total",
-    "calculate_sufficient_n_for_population_total",
-    "calculate_sufficient_n_for_mean",
-]
-
